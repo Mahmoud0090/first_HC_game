3679191e2a1ad317862297c08f3766254ce810e0
Mahmoud0090 <mmh.00090@gmail.com>
2022-09-19 23:42:06 +0300

    particle effects

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System;
 
 public class Obstacle : MonoBehaviour
 {
     public float speed = 10f;
     public TextMeshProUGUI text;
+    public GameObject winParticle;
+    public Transform winParticlePos;
+    public GameObject loseParticle;
+
 
     void Start()
     {
+        try
+        {
+            winParticlePos = GameObject.Find("WinParticlePos").transform;
+        }
+
+        catch(Exception e)
+        {
+            print(e.Message);
+        }
+       
         text = GameObject.Find("scoreText").GetComponent<TextMeshProUGUI>();
     }
 
@@ -24,15 +39,27 @@ public class Obstacle : MonoBehaviour
     {
         if(collision.gameObject.name == "Player")
         {
+            Instantiate(loseParticle, collision.gameObject.transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Invoke("ReloadScene", 2);
         }
         if(collision.gameObject.name == "Out")
         {
+            if (winParticlePos == null)
+            {
+                ReloadScene();
+                return;
+            }
+            Instantiate(winParticle, winParticlePos.position, Quaternion.identity);
             int newScore = int.Parse(text.text) + 1;
             text.text = newScore.ToString();
             Destroy(gameObject);
         }
 
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
